0dd250536ffde513814f2782e4293fd11ac556bc
NsikanPatrick <102978659+NsikanPatrick@users.noreply.github.com>
2022-05-28 12:37:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 class Student:
-    # [assignment] Skeleton class. Add your code here
-    def __init__(self):
-        pass
-
-#Instantiate the class
-
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
+        
+    def change_name(self, new_name):
+        return new_name
+        
+    def change_age(self, new_age):
+        int_age = int(new_age)
+        return int_age
+        
+    def add_tracks(self, added_tracks):
+        new_tracks = added_tracks
+        return new_tracks
+        
+    def get_score(self):
+        return self.score
+        
+        
+# Instantiating the class with initial values
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
-# Expected methods
-Bob.change_name("Peter")
-Bob.change_age(34)
-Bob.add_track("UI/UX")
-Bob.get_score()
+#Providing unique values to print statements   
+print(Bob.change_name("Peter"))
+print(Bob.change_age(34))
+print(Bob.add_tracks("UI/UX"))
+print(Bob.get_score())
